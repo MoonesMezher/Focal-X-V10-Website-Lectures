8e5d7f03e6374d1da1f790a9eff056d29dc53b22
MoonesMezher <moonesmezher050@gmail.com>
2026-06-08 21:40:25 +0300

    l28

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SITE_STATIC_CACHE_NAME = "pwa-cache-v23";
-const SITE_DYNAMIC_CACHE_NAME = "pwa-cache-dynamic-v23";
+const SITE_STATIC_CACHE_NAME = "pwa-cache-v24";
+const SITE_DYNAMIC_CACHE_NAME = "pwa-cache-dynamic-v24";
 
 const assets = [
     "/",
